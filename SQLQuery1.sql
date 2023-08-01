d109d41cdd4ec26de010b3f79fa1c6096cae2668
KUNCHALA SREEEKANTH REDDY <103916336+sreekanth419@users.noreply.github.com>
2023-08-01 03:05:09 -0700

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,18 @@
+create database mssqllife
+use mssqllife
+
+create database DrNavan
+create database rkSreekanth
+create database sreeja
+create database kvnr
+use rkSreekanth
+use DrNavan
+use kvnr
+
+use mssqllife
+use sreeja
+drop database sreeja
+
+alter database kvnr modify name="kvnReddy"
+alter database mssqllife modify name="MSSqlServer"
+use MSSqlServer
