0df2aa1d2b65669d914b44092380e3364524e896
sreekanth419 <103916336+sreekanth419@users.noreply.github.com>
2023-08-01 15:11:50 -0700

    sqlquery execution

---
+++ SQLQuery2.sql
@@ -0,0 +1,5 @@
+
+create table student(id int, name varchar(123),age int,joindate datetime)
+insert into student values(101,'sree',25,'10-12-2023'),(102,'sravani',23,'12-10-2012'),(103,'archana',20,'10-09-2020')
+select * from student
+select * from student where name='archana'
